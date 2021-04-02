20b99cb6d4d5bd2aeb0fa43b53dfb9424689d7a2
Gilles Villeneuve <indio_bass@msn.com>
2021-04-02 16:51:37 -0300

    Adicionar arquivos de projeto.

---
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public abstract class Entidade //abtract = ninguém instanciará minha classe
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public ICollection<Pedido> Pedidos { get; set; } // uma instância de Usuário pode ter nenhum ou muitos pedidos
+
+
+    }
+}
