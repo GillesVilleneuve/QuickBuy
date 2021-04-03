b1a5bf57a535200ae3378df58ac178e58d62fad8
Gilles Villeneuve <indio_bass@msn.com>
2021-04-03 17:27:48 -0300

    Vamos entrar na aula 76.

---
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LoginCOmponent = void 0;
+var LoginCOmponent = /** @class */ (function () {
+    function LoginCOmponent() {
+    }
+    return LoginCOmponent;
+}());
+exports.LoginCOmponent = LoginCOmponent;
+//# sourceMappingURL=login.component.js.map
