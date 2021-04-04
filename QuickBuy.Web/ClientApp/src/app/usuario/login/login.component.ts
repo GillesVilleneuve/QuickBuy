7edddeae1fab213c4c90ef112f9398912ad222e1
Gilles Villeneuve <indio_bass@msn.com>
2021-04-03 21:36:58 -0300

    Melhoria formato das mensagens de validação bootstrap  e ngGlass(Angular)

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from "@angular/core"
+import { Usuario } from "../../modelo/usuario";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
-  styleUrls: "./login.component.css"
+  styleUrls: ["./login.component.css"]
 
 })
 export class LoginComponent {
+  public usuario;
+  
+
+  constructor() {
+
+    this.usuario = new Usuario();
+  }
 
 
+  entrar() {
+
+    if (this.usuario.email == "leo@leo.com" && this.usuario.senha == "123456") {
+    
+    }
+
+  }
+
+  
+  
 }
